1c2ad10ce9ea73857d02e370b47d8f6bc897e3e4
yogaswara <gistha.yogaswara@gmail.com>
2016-01-22 16:26:55 +0700

    update seo share fb + update slider

--- partials/defaultcss.blade.php
+++ partials/defaultcss.blade.php
@@ -1,6 +1,6 @@
     {{favicon()}}
     
-	{{generate_theme_css('playgames/assets/css/bootstrap.min.css')}}
+    {{generate_theme_css('playgames/assets/css/bootstrap.min.css')}}
     {{generate_theme_css('playgames/assets/css/styles.css')}}
     {{generate_theme_css('playgames/assets/css/jquery.bxslider.css')}}
     {{generate_theme_css('playgames/assets/css/font-awesome.min.css')}}
--- partials/slider.blade.php
+++ partials/slider.blade.php
@@ -3,11 +3,11 @@
 		<ul class="bxslider">
 			@foreach (slideshow() as $val)
 			<li>
-				@if($val->text == '')
-                <a href="#">
-                @else
-                <a href="{{filter_link_url($val->text)}}" target="_blank">
-                @endif
+				@if(!empty($val->url))
+				<a href="{{filter_link_url($val->url)}}" target="_blank">
+				@else
+				<a href="#">
+				@endif
 					{{HTML::image(slide_image_url($val->gambar), 'slide banner',array('class'=>'slide_gbr'))}}
 				</a>
 			</li>
